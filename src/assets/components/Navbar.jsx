7d6f4ed8e25ac229d5f61007c72dfa877c8a8038
MOROUNDIYA <moroundiya@outlook.com>
2024-09-10 02:59:17 +0100

    Splitting into components

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
 	return (
 		<nav
 			className={`flex justify-between uppercase items-center w-full z-10 top-0 left-0 px-3 py-4 lg:py-5 lg:px-12 xl:px-20 2xl:py-10 font-lexend transition-all ease-in-out duration-700 ${
-				scroll ? "bg-white fixed shadow-2xl z-50" : "bg-transparent absolute"
+				scroll ? "bg-white fixed shadow z-50" : "bg-transparent absolute"
 			}`}>
 			<Icon
 				icon="tabler:menu-4"
