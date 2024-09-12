75e83a7dfe2654991162b0be91248adb9497bce2
Moroundiya <moroundiya@outlook.com>
2024-09-12 19:39:44 +0100

    Testimonial functionality

--- src/assets/components/Testimonial.jsx
+++ src/assets/components/Testimonial.jsx
@@ -7,6 +7,12 @@ import testiProfile from "../images/testi_profile.jpg";
 import testiQuote from "../images/quote.png";
 import { Icon } from "@iconify/react";
 
+import "swiper/css";
+import "swiper/css/navigation";
+// import required modules
+import { Navigation } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const Testimonial = () => {
 	const slideRef = useRef();
@@ -39,69 +45,132 @@ export const Testimonial = () => {
 			</h1>
 
 			<div className="w-full flex px-3 h-auto rounded-3xl overflow-hidden lg:px-0 xl:w-11/12 2xl:w-4/5 mx-auto bg-testiBg bg-contain bg-right bg-no-repeat bg-[#F5F7EB]">
-				<img
-					src={testiImg}
-					className="hidden lg:block h-full"
-					alt=""
-				/>
-				<img
-					src={testiShape}
-					className="hidden lg:block h-full -ms-4"
-					alt=""
-				/>
+				<div className="w-3/6">
+					<img
+						src={testiImg}
+						className="hidden lg:block h-full w-full"
+						alt=""
+					/>
+				</div>
+				<div className="w-[90px]">
+					<img
+						src={testiShape}
+						className="hidden lg:block h-full -ms-4 "
+						alt=""
+					/>
+				</div>
 
-				<div className="px-3 py-6 lg:p-10 relative">
+				<div className="px-3 py-6 lg:p-8 xl:p-10 relative w-full lg:w-full xl:w-4/6">
 					<img
 						src={testiQuote}
 						className="absolute bottom-16 right-3 lg:right-20 lg:bottom-24 h-[60px] lg:h-[80px] xl:h-auto xl:bottom-6"
 						alt=""
 					/>
-					<p className="italic text-md text-[#54595F] mb-12 leading-[1.8] lg:leading-[1.8] lg:text-xl">
-						“Diversity is a cornerstone of our farming philosophy. We carefully
-						select a wide range of crops, ensuring a varied and balanced
-						ecosystem. This not only helps to naturally deter pests and diseases
-						but also promotes soil health by varying the nutrient demands of
-						different plants.”
-					</p>
-					<div className="flex items-center">
-						<img
-							src={testiProfile}
-							className="rounded-full h-[45px] lg:h-[60px]"
-							alt=""
-						/>
-						<div className="ms-3">
-							<p className="text-black text-lg lg:text-2xl font-semibold">
-								Angelina Margret
-							</p>
-							<p className="text-[#017D03] text-sm">Customer of Our Shop</p>
-						</div>
-					</div>
+					<div className="">
+						<Swiper
+							navigation={true}
+							modules={[Navigation]}
+							className="">
+							<SwiperSlide className="">
+								<p className="italic text-md text-[#54595F] mb-12 leading-[1.8] lg:leading-[1.8] lg:text-xl">
+									“Diversity is a cornerstone of our farming philosophy. We
+									carefully select a wide range of crops, ensuring a varied and
+									balanced ecosystem. This not only helps to naturally deter
+									pests and diseases but also promotes soil health by varying
+									the nutrient demands of different plants.”
+								</p>
+								<div className="flex items-center">
+									<img
+										src={testiProfile}
+										className="rounded-full h-[45px] lg:h-[60px]"
+										alt=""
+									/>
+									<div className="ms-3">
+										<p className="text-black text-lg lg:text-2xl font-semibold">
+											Angelina Margret
+										</p>
+										<p className="text-[#017D03] text-sm">
+											Customer of Our Shop
+										</p>
+									</div>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<p className="italic text-md text-[#54595F] mb-12 leading-[1.8] lg:leading-[1.8] lg:text-xl">
+									“Diversity is a cornerstone of our farming philosophy. We
+									carefully select a wide range of crops, ensuring a varied and
+									balanced ecosystem. This not only helps to naturally deter
+									pests and diseases but also promotes soil health by varying
+									the nutrient demands of different plants.”
+								</p>
+								<div className="flex items-center">
+									<img
+										src={testiProfile}
+										className="rounded-full h-[45px] lg:h-[60px]"
+										alt=""
+									/>
+									<div className="ms-3">
+										<p className="text-black text-lg lg:text-2xl font-semibold">
+											Angelina Margret
+										</p>
+										<p className="text-[#017D03] text-sm">
+											Customer of Our Shop
+										</p>
+									</div>
+								</div>
+							</SwiperSlide>
+							<SwiperSlide>
+								<p className="italic text-md text-[#54595F] mb-12 leading-[1.8] lg:leading-[1.8] lg:text-xl">
+									“Diversity is a cornerstone of our farming philosophy. We
+									carefully select a wide range of crops, ensuring a varied and
+									balanced ecosystem. This not only helps to naturally deter
+									pests and diseases but also promotes soil health by varying
+									the nutrient demands of different plants.”
+								</p>
+								<div className="flex items-center">
+									<img
+										src={testiProfile}
+										className="rounded-full h-[45px] lg:h-[60px]"
+										alt=""
+									/>
+									<div className="ms-3">
+										<p className="text-black text-lg lg:text-2xl font-semibold">
+											Angelina Margret
+										</p>
+										<p className="text-[#017D03] text-sm">
+											Customer of Our Shop
+										</p>
+									</div>
+								</div>
+							</SwiperSlide>
+						</Swiper>
 
-					<div className="flex mt-10 space-x-5 lg:space-x-5 lg:mt-16">
-						<button
-							className={`${
-								!handleSlide.isFirst
-									? "bg-[#017d033e] p-2 lg:p-3 rounded-full cursor-pointer"
-									: "bg-[#017D03] p-2 lg:p-3 rounded-full cursor-pointer"
-							}`}
-							onClick={() => slideRef.current.swiper.slidePrev()}>
-							<Icon
-								icon="ion:arrow-back"
-								className="text-white rounded-full text-[1.3rem] lg:text-2xl"
-							/>
-						</button>
-						<button
-							className={`${
-								!handleSlide.isLast
-									? "bg-[#017d033e] p-2 lg:p-3 rounded-full cursor-pointer"
-									: "bg-[#017D03] p-2 lg:p-3 rounded-full cursor-pointer"
-							}`}
-							onClick={() => slideRef.current.swiper.slideNext()}>
-							<Icon
-								icon="ion:arrow-forward"
-								className="text-white rounded-full text-[1.3rem] lg:text-2xl"
-							/>
-						</button>
+						<div className="flex mt-10 space-x-5 lg:space-x-5 lg:mt-16">
+							<button
+								className={`${
+									!handleSlide.isFirst
+										? "bg-[#017d033e] p-2 lg:p-3 rounded-full cursor-pointer"
+										: "bg-[#017D03] p-2 lg:p-3 rounded-full cursor-pointer"
+								}`}
+								onClick={() => slideRef.current.swiper.slidePrev()}>
+								<Icon
+									icon="ion:arrow-back"
+									className="text-white rounded-full text-[1.3rem] lg:text-2xl"
+								/>
+							</button>
+							<button
+								className={`${
+									!handleSlide.isLast
+										? "bg-[#017d033e] p-2 lg:p-3 rounded-full cursor-pointer"
+										: "bg-[#017D03] p-2 lg:p-3 rounded-full cursor-pointer"
+								}`}
+								onClick={() => slideRef.current.swiper.slideNext()}>
+								<Icon
+									icon="ion:arrow-forward"
+									className="text-white rounded-full text-[1.3rem] lg:text-2xl"
+								/>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
