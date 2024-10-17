bf270f75645e71dd284c9d8e857dfea683e3b8f3
Moroundiya <moroundiya@outlook.com>
2024-10-17 15:05:55 +0100

    Order Completed Page

--- src/assets/pages/Order.jsx
+++ src/assets/pages/Order.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
-import { Icon } from '@iconify/react/dist/iconify.js'
+import React from "react";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export const Order = () => {
-  return (
+	return (
 		<>
 			<Navbar />
-			<div className="w-full min-h-fit flex flex-col justify-center items-center font-lexend mt-0 py-16 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative top-10 lg:top-[72px]">
+			<div className="w-full min-h-fit flex flex-col justify-center items-center px-4 pb-32 font-lexend mt-0 py-16 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative top-10 lg:top-[72px]">
 				<Icon
 					icon="icon-park-solid:check-one"
 					className="text-[#017D03] text-7xl lg:text-8xl mt-5 lg:mt-10"
@@ -19,14 +19,14 @@ export const Order = () => {
 					Thank you. Your order has been received.
 				</p>
 
-				<div className="border-2 border-dashed border-[#FF9C00] p-5 lg:px-10 my-10 w-4/5 lg:w-2/4 grid grid-cols-2 lg:grid-cols-4 gap-y-5 lg:gap-y-0">
+				<div className="border-2 border-dashed border-[#FF9C00] px-5 py-7 lg:px-10 my-10 w-full lg:w-3/5 grid grid-cols-2 lg:grid-cols-4 gap-y-5 lg:gap-y-0">
 					<div className="flex flex-col justify-center space-y-1">
 						<p className="text-[#767676] text-sm">Order Number</p>
 						<p className="font-semibold">13119</p>
 					</div>
 					<div className="flex flex-col justify-center space-y-1">
 						<p className="text-[#767676] text-sm">Date</p>
-						<p className="font-semibold">27/10/2023</p>
+						<p className="font-semibold">12/10/2024</p>
 					</div>
 					<div className="flex flex-col justify-center space-y-1">
 						<p className="text-[#767676] text-sm">Total</p>
@@ -37,8 +37,46 @@ export const Order = () => {
 						<p className="font-semibold">Direct Bank Transfer</p>
 					</div>
 				</div>
+				<div className="border-2 border-dashed border-[#FF9C00] px-5 py-7 lg:px-10 mb-10 w-full lg:w-3/5">
+					<p className="text-sm text-[#767676]">Order Details</p>
+
+					<div className="flex justify-between items-center mt-5">
+						<p>PRODUCT</p>
+						<p>SUBTOTAL</p>
+					</div>
+					<div className="space-y-2 my-3 border-y border-[#767676c0] text-[#767676] text-sm py-3">
+						<div className="flex justify-between items-center w-full">
+							<p>Zessi Dresses x 2</p>
+							<p>$32.50</p>
+						</div>
+						<div className="flex justify-between items-center w-full">
+							<p>Kirby T-Shirt</p>
+							<p>$20.20</p>
+						</div>
+						<div className="flex justify-between items-center w-full">
+							<p>Zessi Dresses x 2</p>
+							<p>$32.50</p>
+						</div>
+					</div>
+					<div className="flex justify-between items-center mt-5 text-sm">
+						<p>SUBTOTAL</p>
+						<p> $62.40</p>
+					</div>
+					<div className="flex justify-between items-center mt-5 text-sm">
+						<p>SHIPPING</p>
+						<p>FREE</p>
+					</div>
+					<div className="flex justify-between items-center mt-5 text-sm">
+						<p>VAT</p>
+						<p>$0.00</p>
+					</div>
+					<div className="flex justify-between items-center mt-5 font-bold text-lg">
+						<p>TOTAL</p>
+						<p>$130.30</p>
+					</div>
+				</div>
 			</div>
 			<Footer />
 		</>
 	);
-}
+};
