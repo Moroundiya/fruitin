9bc93f464e250891b53b4f63b71ddc0955f80ee6
Moroundiya <moroundiya@outlook.com>
2024-10-28 01:29:09 +0100

    Homepage Animation

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -8,9 +8,19 @@ import { Link } from "react-router-dom";
 import { CartSidebar } from "./CartSidebar";
 import { MobileMenu } from "./MobileMenu";
 import { ProductContext } from "../../App";
+import { motion } from "framer-motion";
 
 export const Navbar = () => {
-	const { cartItems} = useContext(ProductContext);
+	const {
+		cartItems,
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
+	} = useContext(ProductContext);
 	const [scroll, setScroll] = useState(false);
 	const currentpath = window.location.pathname;
 
@@ -21,10 +31,7 @@ export const Navbar = () => {
 		window.scrollY > 1 ? setScroll(true) : setScroll(false);
 	};
 
-	useEffect(() => {
-		// console.log(currentpath);
-	}, [scroll, currentpath, showCart, showMenu]);
-
+	useEffect(() => {}, [scroll, currentpath, showCart, showMenu]);
 	return (
 		<>
 			<MobileMenu
@@ -37,44 +44,79 @@ export const Navbar = () => {
 				showMenu={showMenu}
 			/>
 			<nav
-				className={`flex justify-between uppercase items-center w-full z-40 top-0 left-0 px-2.5 py-3 lg:px-12 xl:px-20 font-lexend transition-all ease-in-out duration-700 ${scroll || currentpath !== "/"
-					? "bg-white fixed shadow z-40 lg:py-3"
-					: "bg-transparent absolute lg:py-4"
-					}`}>
-				<Icon
-					icon="tabler:menu-4"
-					className={`text-[2.1rem] cursor-pointer lg:hidden ${scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
+				className={`flex justify-between uppercase items-center w-full z-40 top-0 left-0 px-2.5 py-3 lg:px-12 xl:px-20 font-lexend transition-all ease-in-out duration-700 overflow-x-hidden ${
+					scroll || currentpath !== "/"
+						? "bg-white fixed shadow z-40 lg:py-3"
+						: "bg-transparent absolute lg:py-4"
+				}`}>
+				<motion.div
+					className="lg:hidden"
+					initial={{ x: -50, opacity: 0 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						transition: {
+							type: "spring",
+							duration: 2,
+							bounce: 0.6,
+						},
+					}}>
+					<Icon
+						icon="tabler:menu-4"
+						className={`text-[2.1rem] cursor-pointer ${
+							scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
 						} `}
-					onClick={() => setShowMenu(true)}
-				/>
-				<img
+						onClick={() => setShowMenu(true)}
+					/>
+				</motion.div>
+				<motion.img
 					src={scroll || currentpath !== "/" ? footerLogo : logoWhite}
 					className="h-8 lg:h-12 2xl:h-14"
 					alt=""
+					variants={zoomUp}
+					initial="hidden"
+					animate="visible"
 				/>
-				<div
-					className={`hidden text-lg ${scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
-						} space-x-9 lg:block`}>
+				<motion.div
+					className={`hidden text-lg ${
+						scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
+					} space-x-9 lg:block`}
+					variants={bounceDown}
+					initial="hidden"
+					animate="visible">
 					<Link to="/">Home</Link>
 					<Link to="/about-us">About</Link>
 					<Link to="/shop">Shop</Link>
 					<Link to="/faq">FAQ</Link>
 					<Link to="/contact">Contact</Link>
-				</div>
-				<div className="flex justify-center items-center">
+				</motion.div>
+				<motion.div
+					className="flex justify-center items-center"
+					initial={{ x: 50, opacity: 0 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						transition: {
+							type: "spring",
+							duration: 2,
+							bounce: 0.5,
+						},
+					}}>
 					<Link
 						to="#"
 						className="relative"
 						onClick={() => setShowCart(true)}>
 						<div
-							className={`absolute ${scroll || currentpath !== "/" ? "bg-[#FF9C00]" : "bg-[#017D03]"
-								} text-white rounded-full w-4 h-4 overflow-hidden flex justify-center items-center text-[0.65rem] font-semibold shadow-xl right-0`}>
+							className={`absolute ${
+								scroll || currentpath !== "/" ? "bg-[#FF9C00]" : "bg-[#017D03]"
+							} text-white rounded-full w-4 h-4 overflow-hidden flex justify-center items-center text-[0.65rem] font-semibold shadow-xl right-0`}>
 							{cartItems}
 						</div>
 						<Icon
 							icon="typcn:shopping-cart"
-							className={`text-[2.1rem] lg:text-4xl cursor-pointer ${scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
-								}`}
+							className={`text-[2.1rem] lg:text-4xl cursor-pointer ${
+								scroll || currentpath !== "/" ? "text-[#017D03]" : "text-white"
+							}`}
 						/>
 					</Link>
 					<a
@@ -86,7 +128,7 @@ export const Navbar = () => {
 							className="text-white text-2xl pe-0"
 						/>
 					</a>
-				</div>
+				</motion.div>
 			</nav>
 		</>
 	);
