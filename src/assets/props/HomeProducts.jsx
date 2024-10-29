ecd2d91d2fa0490be9fe4c9557bf6bd4a527e85e
Moroundiya <moroundiya@outlook.com>
2024-10-29 18:44:44 +0100

    Homepage Animation

--- src/assets/props/HomeProducts.jsx
+++ src/assets/props/HomeProducts.jsx
@@ -12,12 +12,16 @@ export const HomeProducts = ({ price, desc, name, type, img }) => {
 		childrenVariants,
 		parentVariant,
 		bounceUp,
+		zoomUp,
 	} = useContext(ProductContext);
 
 	return (
-		<motion.div className="bg-white rounded-[20px] shadow-lg p-1.5 lg:p-4 flex justify-center items-center flex-col cursor-pointer"
-		initial={{}}
-		>
+		<motion.div
+			className="bg-white rounded-[20px] shadow-lg p-1.5 lg:p-4 flex justify-center items-center flex-col cursor-pointer"
+			variants={zoomUp}
+			initial="hidden"
+			whileInView="visible"
+			key={price}>
 			<div className='w-full bg-[#F7F7F7] rounded-[20px] flex justify-center items-center overflow-hidden relative cursor-pointer group after:content-[""] after:w-full after:h-full after:bg-[d#017d0346] after:absolute after:top-0 after:left-0'>
 				<p className="bg-[#017D03] text-white px-2.5 lg:px-4 rounded-md py-0.5 text-[10px] lg:text-[13px] absolute top-3 left-3 font-light">
 					{type}
@@ -37,9 +41,9 @@ export const HomeProducts = ({ price, desc, name, type, img }) => {
 				<Link
 					to="/shop"
 					className={`text-[0.7rem] lg:text-[0.85rem] bg-[#017D03] xl:text-[0.9rem] flex justify-center items-center my-4 lg:mt-3 lg:mb-4  text-white px-2.5 py-1.5 lg:px-4 lg:py-2  rounded-full hover:text-white overflow-hidden relative transition-all duration-700 z-10 after:transition-all after:duration-700 after:origin-bottom  after:content-[""] after:absolute after:top-0 after:left-0 after:-z-10 after:w-full after:h-full after:bg-[#FF9C00] after:scale-y-0 after:hover:scale-y-[1]`}>
-					<span className="me-1 lg:me-2 ps-1">Add to Cart</span>
+					<span className="me-1 lg:me-2 ps-1">Shop Now</span>
 					<Icon
-						icon="icons8:add-shopping-cart"
+						icon="icons8:shopping-cart"
 						className="text-xl lg:text-3xl"
 					/>
 				</Link>
