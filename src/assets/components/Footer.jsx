0c490ec4fc13fd69f758a341cfa2155572c536e3
Moroundiya <moroundiya@outlook.com>
2024-11-01 00:42:38 +0100

    Homepage Animation

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useContext } from "react";
 import footerBg1 from "../images/footer_shape_1.png";
 import footerBg2 from "../images/footer_shape_2.png";
 import footerEmail from "../images/email.svg";
@@ -7,10 +7,21 @@ import footerLogo from "../images/logo-footer.svg";
 import paymentMethod from "../images/payment-method.png";
 import { Icon } from "@iconify/react";
 import titleIcon from "../images/title_icon.svg";
+import { AnimatePresence, motion } from "framer-motion";
+import { ProductContext } from "../../App";
 
 export const Footer = () => {
+	const {
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
+	} = useContext(ProductContext);
 	return (
-		<footer className=" bg-[#002D00] h-auto pt-12 lg:pt-20 relative font-lexend">
+		<footer className=" bg-[#002D00] h-auto pt-12 lg:pt-20 relative font-lexend overflow-x-hidden">
 			<img
 				src={footerBg1}
 				className="absolute top-0 left-0 h-auto lg:h-full pointer-events-none"
@@ -24,17 +35,28 @@ export const Footer = () => {
 			<div className="w-full h-full justify-center px-3 items-center lg:px-12 xl:px-0 xl:w-11/12 2xl:w-4/5 lg:mx-auto relative z-30">
 				<div className="w-full lg:flex justify-between items-center">
 					<div className="w-full lg:w-1/2 justify-center lg:justify-start flex items-center space-x-5 mb-6 lg:mb-0">
-						<img
+						<motion.img
 							src={footerEmail}
 							alt=""
 							className="h-10 lg:h-auto"
+							variants={zoomUp}
+							initial="hidden"
+							whileInView="visible"
 						/>
-						<p className="text-xl text-center sm:text-left lg:text-[1.8rem] leading-[1.2] text-white lg:w-4/5 xl:w-3/5 font-semibold">
+						<motion.p
+							className="text-xl text-center sm:text-left lg:text-[1.8rem] leading-[1.2] text-white lg:w-4/5 xl:w-3/5 font-semibold"
+							variants={bounceDown}
+							initial="hidden"
+							whileInView="visible">
 							Sign Up to Get Updates & News About Us.
-						</p>
+						</motion.p>
 					</div>
 
-					<form className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+					<motion.form
+						className="w-full lg:w-1/2 flex justify-center lg:justify-end"
+						variants={bounceUp}
+						initial="hidden"
+						whileInView="visible">
 						<input
 							type="email"
 							required
@@ -48,96 +70,132 @@ export const Footer = () => {
 							className='bg-[#017D03] min-w-fit text-[0.8rem] text-white uppercase rounded-lg ms-2 lg:text-sm px-2 lg:px-4 z-30 overflow-hidden relative transition-all duration-700 after:transition-all after:duration-700 after:origin-bottom after:content-[""] after:absolute after:top-0 after:left-0 after:-z-10 after:w-full after:h-full after:bg-[#FF9C00] after:scale-y-0 after:hover:scale-y-[1]'>
 							Subscribe
 						</button>
-					</form>
+					</motion.form>
 				</div>
-				<hr className="h-px my-10 lg:my-16 bg-[#017d0380] border-0" />
+				<motion.hr
+					className="h-px my-10 lg:my-16 bg-[#017d0380] border-0"
+					variants={bounceDown}
+					initial="hidden"
+					whileInView="visible"
+				/>
 				<div className="lg:flex justify-center pb-20 space-y-10 lg:space-y-0">
 					<div className="lg:w-1/3">
-						<img
+						<motion.img
 							src={footerLogo}
 							alt=""
 							className="h-10 lg:h-auto"
+							variants={zoomUp}
+							initial="hidden"
+							whileInView="visible"
 						/>
-						<p className="text-[#B5B5B5] my-5 lg:my-10 lg:leading-[1.7] text-[1rem] font-light lg:w-4/5">
+						<motion.p
+							className="text-[#B5B5B5] my-5 lg:my-10 lg:leading-[1.7] text-[1rem] font-light lg:w-4/5"
+							variants={slideLeft}
+							initial="hidden"
+							whileInView="visible">
 							We provide specialized winterization services to safeguard your
 							pool during the off-season, and when spring arrives, we handle the
 							thorough opening process.
-						</p>
+						</motion.p>
 
-						<div className="flex space-x-2">
-							<div className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]">
+						<motion.div
+							className="flex space-x-2"
+							variants={parentVariant}
+							initial="hidden"
+							whileInView="visible">
+							<motion.div
+								className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]"
+								variants={childrenVariants}>
 								<Icon
 									icon="ri:facebook-fill"
 									className=""
 								/>
-							</div>
-							<div className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]">
+							</motion.div>
+							<motion.div
+								className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]"
+								variants={childrenVariants}>
 								<Icon icon="mdi:twitter" />
-							</div>
-							<div className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]">
+							</motion.div>
+							<motion.div
+								className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]"
+								variants={childrenVariants}>
 								<Icon icon="mdi:linkedin" />
-							</div>
-							<div className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]">
+							</motion.div>
+							<motion.div
+								className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl transition-all duration-[0.6s] ease-in-out hover:bg-[#017D03]"
+								variants={childrenVariants}>
 								<Icon icon="ic:baseline-whatsapp" />
-							</div>
-						</div>
+							</motion.div>
+						</motion.div>
 					</div>
 					<div className="lg:w-1/4">
-						<div className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg">
+						<motion.div
+							className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg"
+							variants={slideRight}
+							initial="hidden"
+							whileInView="visible">
 							<img
 								src={titleIcon}
 								className=""
 								alt=""
 							/>
 							<p className="text-xl lg:text-[1.38rem]">Categories</p>
-						</div>
-						<div className="space-y-2.5 lg:space-y-3.5">
-							<a
-								href="#"
-								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
-								<Icon
-									icon="tabler:arrow-right"
-									className="text-xl"
-								/>
-								<span>Fresh Vegetable</span>
-							</a>
-							<a
-								href="#"
-								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
-								<Icon
-									icon="tabler:arrow-right"
-									className="text-xl"
-								/>
-								<span>Natural Fruits</span>
-							</a>
-							<a
-								href="#"
-								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
-								<Icon
-									icon="tabler:arrow-right"
-									className="text-xl"
-								/>
-								<span>Dairy Products</span>
-							</a>
-							<a
-								href="#"
-								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
-								<Icon
-									icon="tabler:arrow-right"
-									className="text-xl"
-								/>
-								<span>Tea & Coffee</span>
-							</a>
-							<a
-								href="#"
-								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
-								<Icon
-									icon="tabler:arrow-right"
-									className="text-xl"
-								/>
-								<span>Meat and Fish</span>
-							</a>
-						</div>
+						</motion.div>
+						<AnimatePresence>
+							<motion.ul
+								className="space-y-2.5 lg:space-y-3.5"
+								variants={parentVariant}
+								initial="hidden"
+								whileInView="visible"
+								exit="hidden">
+								<motion.li
+									className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]"
+									variants={childrenVariants}>
+									<Icon
+										icon="tabler:arrow-right"
+										className="text-xl"
+									/>
+									<span>Fresh Vegetable</span>
+								</motion.li>
+
+								<motion.li
+									variants={childrenVariants}
+									className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
+									<Icon
+										icon="tabler:arrow-right"
+										className="text-xl"
+									/>
+									<span>Natural Fruits</span>
+								</motion.li>
+								<motion.li
+									variants={childrenVariants}
+									className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
+									<Icon
+										icon="tabler:arrow-right"
+										className="text-xl"
+									/>
+									<span>Dairy Products</span>
+								</motion.li>
+								<motion.li
+									variants={childrenVariants}
+									className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
+									<Icon
+										icon="tabler:arrow-right"
+										className="text-xl"
+									/>
+									<span>Tea & Coffee</span>
+								</motion.li>
+								<motion.li
+									variants={childrenVariants}
+									className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 transition-all ease-in-out duration-500 cursor-pointer hover:text-[#FF9C00]">
+									<Icon
+										icon="tabler:arrow-right"
+										className="text-xl"
+									/>
+									<span>Meat and Fish</span>
+								</motion.li>
+							</motion.ul>
+						</AnimatePresence>
 					</div>
 					<div className="lg:w-1/4">
 						<div className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg">
