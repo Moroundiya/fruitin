ee245e2cb22de8d89fa45ea85ecff3daef63c4e8
Moroundiya <moroundiya@outlook.com>
2024-11-03 00:30:53 +0100

    Homepage Animation

--- src/assets/pages/Shop.jsx
+++ src/assets/pages/Shop.jsx
@@ -1,18 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navbar } from "../components/Navbar";
 import { AllProducts } from "../components/AllProducts";
 import { Footer } from "../components/Footer";
+import { motion } from "framer-motion";
+import { ProductContext } from "../../App";
 
 export const Shop = () => {
-		useEffect(() => {
-			window.scrollTo(0, 0);
-		}, []);
+	const {
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
+	} = useContext(ProductContext);
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
 	return (
-		<div className="mt-[50px] lg:mt-[70px]">
+		<motion.div className="mt-[50px] lg:mt-[70px] overflow-x-hidden">
 			<Navbar />
 			<AllProducts />
 			<Footer />
-		</div>
+		</motion.div>
 	);
 };
