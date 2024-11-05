28489cf67a029999a8ef65a333db03c99ce1818a
Moroundiya <moroundiya@outlook.com>
2024-11-05 21:55:27 +0100

    Cart Animation

--- src/assets/components/CartSingleItem.jsx
+++ src/assets/components/CartSingleItem.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { Icon } from "@iconify/react/dist/iconify.js";
-import React from "react";
+import { motion } from "framer-motion";
+import React, { useContext } from "react";
+import { ProductContext } from "../../App";
 export const CartSingleItem = ({
 	img,
 	name,
@@ -13,23 +15,52 @@ export const CartSingleItem = ({
 	reduceQuantity,
 	overall,
 }) => {
+	const {
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
+	} = useContext(ProductContext);
+
 	return (
-		<tr className="text-center border border-[#FF9C00] font-light block w-full lg:table-row">
+		<tr className="text-center border border-[#FF9C00] font-light block w-full lg:table-row overflow-x-hidden">
 			<td className="w-full lg:w-auto flex justify-end items-center px-5 lg:px-0 lg:table-cell border-b border-[#FF9C00] lg:border-b-0 before:content-['Image'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold">
-				<img
+				<motion.img
 					src={img}
 					className="lg:mx-auto h-20"
 					alt=""
+					variants={zoomUp}
+					initial="hidden"
+					whileInView="visible"
 				/>
 			</td>
-			<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Product_Name'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+			<motion.td
+				className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Product_Name'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+				variants={slideRight}
+				initial="hidden"
+				whileInView="visible">
 				{name}
-			</td>
-			<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Price'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+			</motion.td>
+			<motion.td
+				className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Price'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+				variants={slideRight}
+				initial="hidden"
+				whileInView="visible">
 				${price}
-			</td>
-			<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Quantity'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
-				<div className="flex justify-center items-center space-x-4 ">
+			</motion.td>
+			<motion.td
+				className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Quantity'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+				variants={slideRight}
+				initial="hidden"
+				whileInView="visible">
+				<motion.div
+					className="flex justify-center items-center space-x-4 "
+					variants={slideLeft}
+					initial="hidden"
+					whileInView="visible">
 					<button
 						onClick={() => reduceQuantity(index)}
 						className="border px-1.5 py-0.5 border-gray-300 font-semibold rounded-sm hover:bg-[#017D03] hover:text-white transition-all ease-in-out duration-300">
@@ -43,11 +74,15 @@ export const CartSingleItem = ({
 						className="border px-1.5 py-0.5 border-gray-300 font-semibold rounded-sm hover:bg-[#017D03] hover:text-white transition-all ease-in-out duration-300">
 						<Icon icon="iconoir:plus" />
 					</button>
-				</div>
-			</td>
-			<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell relative lg:border-b-0 before:content-['Total'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+				</motion.div>
+			</motion.td>
+			<motion.td
+				className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell relative lg:border-b-0 before:content-['Total'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+				variants={slideRight}
+				initial="hidden"
+				whileInView="visible">
 				${item.total()}
-			</td>
+			</motion.td>
 		</tr>
 	);
 };
