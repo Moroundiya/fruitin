d92c2c5039e81a8c7b013a98e8dac3300101fdc6
Moroundiya <moroundiya@outlook.com>
2024-11-05 21:18:12 +0100

    Cart Animation

--- src/assets/components/CartItem.jsx
+++ src/assets/components/CartItem.jsx
@@ -5,10 +5,22 @@ import productBg2 from "../images/vector_shape_2.png";
 import { Link, useNavigate } from "react-router-dom";
 import { ProductContext } from "../../App";
 import { CartSingleItem } from "./CartSingleItem";
+import { motion } from "framer-motion";
 
 export const CartItem = () => {
-	const { storeCart, setStoreCart, totalPrice, setTotalPrice } =
-		useContext(ProductContext);
+	const {
+		storeCart,
+		setStoreCart,
+		totalPrice,
+		setTotalPrice,
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
+	} = useContext(ProductContext);
 
 	const increaseQuantity = (val) => {
 		setStoreCart(
@@ -42,7 +54,7 @@ export const CartItem = () => {
 	}, [storeCart]);
 
 	return (
-		<div className="w-full min-h-full font-lexend mt-0 py-16 px-3 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative">
+		<div className="w-full min-h-full font-lexend mt-0 py-16 px-3 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative overflow-x-hidden">
 			<img
 				src={productBg1}
 				className="absolute top-0 left-0 pointer-events-none"
@@ -55,7 +67,11 @@ export const CartItem = () => {
 			/>
 			<div className="w-full xl:w-11/12 2xl:w-4/5 mx-auto z-20 relative top-10 lg:top-[72px] ">
 				<div className="flex flex-col justify-center items-center">
-					<div className="flex items-center justify-center lg:justify-start">
+					<motion.div
+						className="flex items-center justify-center lg:justify-start"
+						variants={bounceDown}
+						initial="hidden"
+						whileInView="visible">
 						<img
 							src={titleIcon}
 							className="h-5"
@@ -64,21 +80,29 @@ export const CartItem = () => {
 						<p className="font-lobster text-[#FF9C00] ms-1 text-lg sm:text-xl">
 							Cart
 						</p>
-					</div>
+					</motion.div>
 					<div className="mt-2 mb-3 w-full mx-auto lg:mb-8 lg:mt-5">
-						<h1 className="text-2xl lg:text-3xl font-bold 2xl:text-[2.5rem] 2xl:leading-[1.15] text-center">
+						<motion.h1
+							className="text-2xl lg:text-3xl font-bold 2xl:text-[2.5rem] 2xl:leading-[1.15] text-center"
+							variants={bounceUp}
+							initial="hidden"
+							whileInView="visible">
 							Cart Details
-						</h1>
+						</motion.h1>
 						<table className="table-auto mt-14 w-full border-none">
 							<thead className="border">
-								<tr className="bg-[#017D03] text-white hidden lg:table-row space-y-10">
+								<motion.tr
+									className="bg-[#017D03] text-white hidden lg:table-row space-y-10"
+									variants={bounceDown}
+									initial="hidden"
+									whileInView="visible">
 									<th className="block lg:table-cell p-2.5 lg:p-5">Image</th>
 									<th className="block lg:table-cell ">Product Name</th>
 									<th className="block lg:table-cell">Price</th>
 									<th className="block lg:table-cell">Quantity</th>
 									<th className="block lg:table-cell">Total</th>
 									<th className="block lg:table-cell"></th>
-								</tr>
+								</motion.tr>
 							</thead>
 							<tbody className="block lg:table-row-group space-y-16 lg:space-y-0">
 								{storeCart.map((item, i) => {
@@ -96,13 +120,17 @@ export const CartItem = () => {
 								})}
 							</tbody>
 						</table>
-						<div className="w-full flex justify-end items-center my-5">
+						<motion.div
+							className="w-full flex justify-end items-center my-5"
+							variants={slideLeft}
+							initial="hidden"
+							whileInView="visible">
 							<Link
 								to="/shop"
 								className='bg-[#FF9C00] text-sm text-center capitalize py-3 px-6 rounded-full text-white lg:text-[1rem] z-40 hover:text-white overflow-hidden relative transition-all duration-700 after:transition-all after:duration-700 after:origin-bottom  after:content-[" "] after:absolute after:top-0 after:left-0 after:-z-10 after:w-full after:h-full after:bg-[#017D03] after:scale-y-0 after:hover:scale-y-[1]'>
 								continue shopping
 							</Link>
-						</div>
+						</motion.div>
 
 						<form
 							onSubmit={(e) => {
@@ -110,7 +138,13 @@ export const CartItem = () => {
 							}}
 							className="w-full h-auto flex justify-end my-10">
 							<div className="w-full h-full lg:w-1/2">
-								<p className="text-xl lg:text-3xl font-semibold">Cart Totals</p>
+								<motion.p
+									className="text-xl lg:text-3xl font-semibold"
+									variants={bounceDown}
+									initial="hidden"
+									whileInView="visible">
+									Cart Totals
+								</motion.p>
 								<table className="table-auto w-full mt-2 lg:mt-5">
 									<tr className="">
 										<td className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]">
@@ -168,13 +202,17 @@ export const CartItem = () => {
 									</tr>
 								</table>
 
-								<div className="w-full flex justify-start items-center mt-5 mb-16">
+								<motion.div
+									className="w-full flex justify-start items-center mt-5 mb-16"
+									variants={slideRight}
+									initial="hidden"
+									whileInView="visible">
 									<button
 										type="submit"
 										className='bg-[#017D03] text-sm text-center capitalize py-3 px-6 rounded-full text-white lg:text-[1rem] z-40 hover:text-white overflow-hidden relative transition-all duration-700 after:transition-all after:duration-700 after:origin-bottom  after:content-[" "] after:absolute after:top-0 after:left-0 after:-z-10 after:w-full after:h-full after:bg-[#FF9C00] after:scale-y-0 after:hover:scale-y-[1]'>
 										Proceed to checkout
 									</button>
-								</div>
+								</motion.div>
 							</div>
 						</form>
 					</div>
