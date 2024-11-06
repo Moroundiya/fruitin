fbb753e5d357fecdca2ca583e2ad6eeef5ca1b9c
Moroundiya <moroundiya@outlook.com>
2024-11-06 21:27:03 +0100

    Cart Animation

--- src/assets/components/CheckoutItems.jsx
+++ src/assets/components/CheckoutItems.jsx
@@ -6,6 +6,7 @@ import productBg2 from "../images/vector_shape_2.png";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { ProductContext } from "../../App";
 import { useNavigate } from "react-router";
+import { motion } from "framer-motion";
 
 export const CheckoutItems = () => {
 	const {
@@ -21,6 +22,13 @@ export const CheckoutItems = () => {
 		setNumber,
 		email,
 		number,
+		bounceDown,
+		slideRight,
+		slideLeft,
+		childrenVariants,
+		parentVariant,
+		bounceUp,
+		zoomUp,
 	} = useContext(ProductContext);
 
 	const navigate = useNavigate();
@@ -28,9 +36,8 @@ export const CheckoutItems = () => {
 	useEffect(() => {
 		// console.log("method is " + paymentMethod);
 	}, [username, address, email, number]);
-
 	return (
-		<div className="w-full min-h-full font-lexend mt-0 py-16 px-3 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative">
+		<div className="w-full min-h-full font-lexend mt-0 py-16 px-3 lg:px-12 bg-[#F5F7EB] xl:mx-auto xl:px-0 relative overflow-x-hidden overflow-y-clip">
 			<img
 				src={productBg1}
 				className="absolute top-0 left-0 pointer-events-none"
@@ -43,7 +50,11 @@ export const CheckoutItems = () => {
 			/>
 			<div className="w-full xl:w-11/12 2xl:w-4/5 mx-auto z-20 relative top-10 lg:top-[72px] ">
 				<div className="flex flex-col justify-center items-center">
-					<div className="flex items-center justify-center lg:justify-start">
+					<motion.div
+						className="flex items-center justify-center lg:justify-start"
+						variants={bounceDown}
+						initial="hidden"
+						whileInView="visible">
 						<img
 							src={titleIcon}
 							className="h-5"
@@ -52,11 +63,15 @@ export const CheckoutItems = () => {
 						<p className="font-lobster text-[#FF9C00] ms-1 text-lg sm:text-xl">
 							Checkout
 						</p>
-					</div>
+					</motion.div>
 					<div className="mt-10 mb-3 w-full mx-auto lg:mb-8 lg:mt-12">
-						<h1 className="text-2xl lg:text-4xl font-bold 2xl:leading-[1.15] text-center">
+						<motion.h1
+							className="text-2xl lg:text-4xl font-bold 2xl:leading-[1.15] text-center"
+							variants={bounceUp}
+							initial="hidden"
+							whileInView="visible">
 							Billing Details
-						</h1>
+						</motion.h1>
 
 						<form
 							onSubmit={(e) => {
@@ -64,8 +79,14 @@ export const CheckoutItems = () => {
 							}}
 							className="flex flex-col items-center lg:items-start">
 							<div className="py-10 w-full lg:w-4/6 mx-auto relative z-20">
-								<div className="grid md:grid-cols-2 gap-x-10 gap-y-5">
-									<div className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]">
+								<motion.div
+									className="grid md:grid-cols-2 gap-x-10 gap-y-5"
+									variants={parentVariant}
+									initial="hidden"
+									whileInView="visible">
+									<motion.div
+										className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]"
+										variants={childrenVariants}>
 										<Icon
 											icon="basil:user-outline"
 											className="text-3xl text-[#FF9C00]"
@@ -78,9 +99,11 @@ export const CheckoutItems = () => {
 											required
 											onChange={(e) => setUsername(e.target.value)}
 										/>
-									</div>
+									</motion.div>
 
-									<div className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]">
+									<motion.div
+										className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]"
+										variants={childrenVariants}>
 										<Icon
 											icon="ic:outline-email"
 											className="text-2xl text-[#FF9C00]"
@@ -92,8 +115,10 @@ export const CheckoutItems = () => {
 											onChange={(e) => setEmail(e.target.value)}
 											required
 										/>
-									</div>
-									<div className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]">
+									</motion.div>
+									<motion.div
+										className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]"
+										variants={childrenVariants}>
 										<Icon
 											icon="solar:phone-outline"
 											className="text-2xl text-[#FF9C00]"
@@ -105,9 +130,11 @@ export const CheckoutItems = () => {
 											onChange={(e) => setNumber(e.target.value)}
 											required
 										/>
-									</div>
+									</motion.div>
 
-									<div className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]">
+									<motion.div
+										className="w-full rounded-lg flex items-center h-11 px-2.5 py-2 bg-[#a1a19a4c]"
+										variants={childrenVariants}>
 										<Icon
 											icon="mdi:address-marker-outline"
 											className="text-2xl text-[#FF9C00]"
@@ -119,22 +146,30 @@ export const CheckoutItems = () => {
 											required
 											onChange={(e) => setAddress(e.target.value)}
 										/>
-									</div>
-								</div>
+									</motion.div>
+								</motion.div>
 							</div>
 
-							<h1 className="text-2xl lg:text-3xl mt-5 font-bold 2xl:leading-[1.15] text-center">
+							<motion.h1
+								className="text-2xl lg:text-3xl mt-5 font-bold 2xl:leading-[1.15] text-center"
+								variants={bounceUp}
+								initial="hidden"
+								whileInView="visible">
 								Your Order:
-							</h1>
+							</motion.h1>
 							<table className="table-auto mt-5 w-full border-none">
 								<thead className="border">
-									<tr className="bg-[#017D03] text-white hidden lg:table-row space-y-10">
+									<motion.tr
+										className="bg-[#017D03] text-white hidden lg:table-row space-y-10"
+										variants={bounceDown}
+										initial="hidden"
+										whileInView="visible">
 										<th className="block lg:table-cell p-2.5 lg:p-5">Image</th>
 										<th className="block lg:table-cell ">Product Name</th>
 										<th className="block lg:table-cell">Price</th>
 										<th className="block lg:table-cell">Quantity</th>
 										<th className="block lg:table-cell">Total</th>
-									</tr>
+									</motion.tr>
 								</thead>
 								<tbody className="block lg:table-row-group space-y-16 lg:space-y-0">
 									{storeCart.map((item, i) => {
@@ -143,26 +178,45 @@ export const CheckoutItems = () => {
 												key={i}
 												className="text-center border border-[#FF9C00] font-light block w-full lg:table-row">
 												<td className="w-full lg:w-auto flex justify-end items-center px-5 lg:px-0 lg:table-cell border-b border-[#FF9C00] lg:border-b-0 before:content-['Image'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold">
-													<img
+													<motion.img
 														src={item.img}
 														className="lg:mx-auto h-20"
 														alt=""
+														variants={zoomUp}
+														initial="hidden"
+														whileInView="visible"
 													/>
 												</td>
-												<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Product_Name'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+												<motion.td
+													className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Product_Name'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+													variants={slideRight}
+													initial="hidden"
+													whileInView="visible">
 													{item.name}
-												</td>
-												<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Price'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+												</motion.td>
+												<motion.td
+													className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Price'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+													variants={slideRight}
+													initial="hidden"
+													whileInView="visible">
 													${item.price}
-												</td>
-												<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Quantity'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+												</motion.td>
+												<motion.td
+													className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell border-b relative border-[#FF9C00] lg:border-b-0 before:content-['Quantity'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold"
+													variants={slideRight}
+													initial="hidden"
+													whileInView="visible">
 													<div className="flex justify-center items-center space-x-4 ">
 														<p className="">{item.quantity}</p>
 													</div>
-												</td>
-												<td className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell relative lg:border-b-0 before:content-['Total'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold ">
+												</motion.td>
+												<motion.td
+													className="w-full lg:w-auto flex justify-end items-center px-3 py-3 lg:py-0 lg:px-0 lg:table-cell relative lg:border-b-0 before:content-['Total'] before:lg:hidden before:absolute before:left-3 before:text-black before:font-bold "
+													variants={slideRight}
+													initial="hidden"
+													whileInView="visible">
 													${item.total()}
-												</td>
+												</motion.td>
 											</tr>
 										);
 									})}
@@ -171,33 +225,63 @@ export const CheckoutItems = () => {
 
 							<table className="mt-10 w-full lg:w-1/2">
 								<tr className="">
-									<td className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]">
+									<motion.td
+										className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]"
+										variants={slideRight}
+										initial="hidden"
+										whileInView="visible">
 										Subtotal
-									</td>
-									<td className="border border-[#FF9C00] w-1/2 font-light p-3">
+									</motion.td>
+									<motion.td
+										className="border border-[#FF9C00] w-1/2 font-light p-3"
+										variants={slideLeft}
+										initial="hidden"
+										whileInView="visible">
 										${totalPrice}
-									</td>
+									</motion.td>
 								</tr>
 								<tr className="">
-									<td className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]">
+									<motion.td
+										className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]"
+										variants={slideRight}
+										initial="hidden"
+										whileInView="visible">
 										Shipping Fee
-									</td>
-									<td className="border border-[#FF9C00] w-1/2 font-light px-3 py-4">
+									</motion.td>
+									<motion.td
+										className="border border-[#FF9C00] w-1/2 font-light px-3 py-4"
+										variants={slideLeft}
+										initial="hidden"
+										whileInView="visible">
 										FREE
-									</td>
+									</motion.td>
 								</tr>
 								<tr className="">
-									<td className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]">
+									<motion.td
+										className="border border-[#FF9C00] w-1/3 font-semibold p-3 bg-[#7681811a]"
+										variants={slideRight}
+										initial="hidden"
+										whileInView="visible">
 										Total Order
-									</td>
-									<td className="border border-[#FF9C00] w-1/2 p-3 text-xl font-semibold text-[#017D03]">
+									</motion.td>
+									<motion.td
+										className="border border-[#FF9C00] w-1/2 p-3 text-xl font-semibold text-[#017D03]"
+										variants={slideLeft}
+										initial="hidden"
+										whileInView="visible">
 										${totalPrice}
-									</td>
+									</motion.td>
 								</tr>
 							</table>
 
-							<div className="mt-8 w-full mb-20">
-								<div className="flex mb-2 lg:mb-3 flex-col">
+							<motion.div
+								className="mt-8 w-full mb-20"
+								variants={parentVariant}
+								initial="hidden"
+								whileInView="visible">
+								<motion.div
+									className="flex mb-2 lg:mb-3 flex-col"
+									variants={childrenVariants}>
 									<div className="flex items-center">
 										<input
 											id="default-radio-1"
@@ -225,8 +309,10 @@ export const CheckoutItems = () => {
 										your Order ID as the payment reference. Your order will not
 										be shipped until the funds have cleared in our account.
 									</p>
-								</div>
-								<div className="flex mb-2 lg:mb-3 flex-col">
+								</motion.div>
+								<motion.div
+									className="flex mb-2 lg:mb-3 flex-col"
+									variants={childrenVariants}>
 									<div className="flex items-center">
 										<input
 											defaultChecked=""
@@ -253,8 +339,10 @@ export const CheckoutItems = () => {
 										Please send a check to Store Name, Store Street, Store Town,
 										Store State / County, Store Postcode.
 									</p>
-								</div>
-								<div className="flex mb-2 lg:mb-3 flex-col">
+								</motion.div>
+								<motion.div
+									className="flex mb-2 lg:mb-3 flex-col"
+									variants={childrenVariants}>
 									<div className="flex items-center">
 										<input
 											defaultChecked=""
@@ -280,8 +368,10 @@ export const CheckoutItems = () => {
 										}`}>
 										Pay with cash upon delivery.
 									</p>
-								</div>
-								<div className="flex mb-2 lg:mb-3 flex-col">
+								</motion.div>
+								<motion.div
+									className="flex mb-2 lg:mb-3 flex-col"
+									variants={childrenVariants}>
 									<div className="flex items-center">
 										<input
 											defaultChecked=""
@@ -308,15 +398,19 @@ export const CheckoutItems = () => {
 										Pay via PayPal; you can pay with your credit card if you
 										don’t have a PayPal account.
 									</p>
-								</div>
-								<div className="w-full flex justify-start items-center my-8">
+								</motion.div>
+								<motion.div
+									className="w-full flex justify-start items-center my-8"
+									variants={slideRight}
+									initial="hidden"
+									whileInView="visible">
 									<button
 										type="submit"
 										className='bg-[#017D03] text-sm text-center capitalize py-3 px-6 rounded-full text-white lg:text-[1rem] z-50 hover:text-white overflow-hidden relative transition-all duration-700 after:transition-all after:duration-700 after:origin-bottom  after:content-[" "] after:absolute after:top-0 after:left-0 after:-z-10 after:w-full after:h-full after:bg-[#FF9C00] after:scale-y-0 after:hover:scale-y-[1]'>
 										Place Order
 									</button>
-								</div>
-							</div>
+								</motion.div>
+							</motion.div>
 						</form>
 					</div>
 				</div>
