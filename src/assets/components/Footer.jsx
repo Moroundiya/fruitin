5741bed94c543659db50d33165cb9c3147a94548
Moroundiya <85170711+Moroundiya@users.noreply.github.com>
2026-03-26 02:15:08 +0100

    Update Footer.jsx

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -41,13 +41,13 @@ export const Footer = () => {
 							className="h-10 lg:h-auto"
 							variants={zoomUp}
 							initial="hidden"
-							whileInView="visible"
+							whileInView="visible" viewport={{ once: true }}
 						/>
 						<motion.p
 							className="text-xl text-center sm:text-left lg:text-[1.8rem] leading-[1.2] text-white lg:w-4/5 xl:w-3/5 font-semibold"
 							variants={bounceDown}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							Sign Up to Get Updates & News About Us.
 						</motion.p>
 					</div>
@@ -56,7 +56,7 @@ export const Footer = () => {
 						className="w-full lg:w-1/2 flex justify-center lg:justify-end"
 						variants={bounceUp}
 						initial="hidden"
-						whileInView="visible">
+						whileInView="visible" viewport={{ once: true }}>
 						<input
 							type="email"
 							required
@@ -76,7 +76,7 @@ export const Footer = () => {
 					className="h-px my-10 lg:my-16 bg-[#017d0380] border-0"
 					variants={bounceDown}
 					initial="hidden"
-					whileInView="visible"
+					whileInView="visible" viewport={{ once: true }}
 				/>
 				<div className="lg:flex justify-center pb-20 space-y-10 lg:space-y-0">
 					<div className="lg:w-1/3">
@@ -86,13 +86,13 @@ export const Footer = () => {
 							className="h-10 lg:h-auto"
 							variants={zoomUp}
 							initial="hidden"
-							whileInView="visible"
+							whileInView="visible" viewport={{ once: true }}
 						/>
 						<motion.p
 							className="text-[#B5B5B5] my-5 lg:my-10 lg:leading-[1.7] text-[1rem] font-light lg:w-4/5"
 							variants={slideLeft}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							We provide specialized winterization services to safeguard your
 							pool during the off-season, and when spring arrives, we handle the
 							thorough opening process.
@@ -102,7 +102,7 @@ export const Footer = () => {
 							className="flex space-x-2"
 							variants={parentVariant}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<motion.div
 								className="text-white bg-[#123C12] w-fit p-3 cursor-pointer rounded-lg text-xl hover:transition-all hover:duration-[0.6s] hover:ease-in-out hover:bg-[#017D03]"
 								variants={childrenVariants}>
@@ -133,7 +133,7 @@ export const Footer = () => {
 							className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg"
 							variants={slideRight}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<img
 								src={titleIcon}
 								className=""
@@ -145,7 +145,7 @@ export const Footer = () => {
 							className="space-y-2.5 lg:space-y-3.5"
 							variants={parentVariant}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<motion.li
 								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 cursor-pointer hover:text-[#FF9C00]"
 								variants={childrenVariants}>
@@ -199,7 +199,7 @@ export const Footer = () => {
 							className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg"
 							variants={slideRight}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<img
 								src={titleIcon}
 								className=""
@@ -211,7 +211,7 @@ export const Footer = () => {
 							className="space-y-2.5 lg:space-y-3.5"
 							variants={parentVariant}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<motion.li
 								className="text-[#B5B5B5] text-[1rem] font-light flex items-center space-x-1.5 cursor-pointer hover:text-[#FF9C00]"
 								variants={childrenVariants}>
@@ -264,7 +264,7 @@ export const Footer = () => {
 							className="flex h-[50px] mb-5 lg:mb-10 items-center space-x-2 text-white relative  after:content-[''] after:w-[106px] after:h-[3px] after:absolute after:-bottom-1 after:left-0 after:bg-gradient-to-r after:from-[#017D03] after:to-transparent after:rounded-lg"
 							variants={slideRight}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<img
 								src={titleIcon}
 								className=""
@@ -276,7 +276,7 @@ export const Footer = () => {
 							className="space-y-4"
 							variants={parentVariant}
 							initial="hidden"
-							whileInView="visible">
+							whileInView="visible" viewport={{ once: true }}>
 							<motion.div
 								className="flex items-center space-x-3 text-[#B5B5B5] font-light"
 								variants={childrenVariants}>
@@ -321,7 +321,7 @@ export const Footer = () => {
 						className="flex flex-col justify-center lg:justify-start items-center mb-3.5 leading-[1.2] text-[0.95rem] lg:text-md lg:mb-0"
 						variants={slideRight}
 						initial="hidden"
-						whileInView="visible">
+						whileInView="visible" viewport={{ once: true }}>
 						<p className="flex items-center">
 							{" "}
 							Copyright{" "}
@@ -336,7 +336,7 @@ export const Footer = () => {
 					<motion.div
 						variants={zoomUp}
 						initial="hidden"
-						whileInView="visible">
+						whileInView="visible" viewport={{ once: true }}>
 						<img
 							src={paymentMethod}
 							alt=""
@@ -347,7 +347,7 @@ export const Footer = () => {
 					<motion.span
 						variants={slideLeft}
 						initial="hidden"
-						whileInView="visible">
+						whileInView="visible" viewport={{ once: true }}>
 						<a
 							href="https://wa.me/qr/4J43PIRULNA5G1"
 							className="text-[0.95rem] lg:text-md lg:mt-0">
